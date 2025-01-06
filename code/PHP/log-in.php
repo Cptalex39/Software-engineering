2958c048f978c3ee007ce175865b5a79cc7b3c2b
Cptalex39 <127256379+Cptalex39@users.noreply.github.com>
2025-01-06 23:26:09 +0100

    Add files via upload

---
+++ code/PHP/log-in.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+ob_start();
+    $servername = "localhost:3306";  // Nome del server MySQL
+    $username = "root";     // Nome utente MySQL
+    $password = "";     // Password MySQL
+    $database = "gamevault";     // Nome del database
+    
+    // Creazione della connessione
+    $conn = new mysqli($servername, $username, $password, $database);
+    
+    // Verifica della connessione
+    if ($conn->connect_error) 
+    {
+        die("Connessione al database fallita: " . $conn->connect_error);
+    }
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+        //variabili del prodotto
+        $u_email=$_POST["e-mail"];
+        $u_password=$_POST["pswd"];
+        $u_password=md5($u_password);
+        if(!empty($u_email)&&!empty($u_password))
+        {
+            $stmt = $conn->query("SELECT * FROM utente WHERE email='$u_email'");
+            $sql="SELECT IdUtente FROM utente WHERE email='$u_email'";
+            if ($stmt->num_rows == 1) 
+            {
+                $row=$stmt->fetch_assoc();
+                $hashed_password=$row["pass"];
+                $stmt1=$conn->prepare($sql);
+                $stmt1->execute();
+                $stmt1->bind_result($user_id);
+                $stmt1->fetch();
+                if($u_password===$hashed_password)
+                {
+                    if($u_email==="admin@gmail.com")
+                    {
+                        echo "Autenticazione riuscita";
+                        header("Location: ../html/adminweb.php");
+                        exit();
+                    }
+                    else
+                    {
+                        $_SESSION['userId'] = $user_id;
+                        echo "Autenticazione riuscita";
+                        header("Location: ../html/Home.php");
+                        exit();
+                    }
+                }
+                else
+                {
+                    echo "Dati inseriti errati. Riprova";
+                    header("Location: ../html/login.html");
+                }
+            }
+            else
+            {
+                echo "Non sei registrato, registrati";
+                exit();
+            }
+        }
+       
+        //aggiungere codice per password dimenticata
+    } 
+    else
+    {
+        echo "Complete tutti i campi";
+        exit();
+    }
+    $conn->close();
+    ob_end_flush();
+?>
--- code/PHP/signup.php
+++ code/PHP/signup.php
@@ -1,7 +1,8 @@
 <?php
+session_start();
  $servername = "localhost:3306";  // Nome del server MySQL
- $username = "admin";     // Nome utente MySQL
- $password = "admin";     // Password MySQL
+ $username = "root";     // Nome utente MySQL
+ $password = "";     // Password MySQL
  $database = "gamevault";     // Nome del database
  
  // Creazione della connessione
@@ -16,20 +17,31 @@
  {
     $u_username=$_POST["username"];
     $u_email=$_POST["e-mail"];
-    $u_password=$_POST["pswd"];
+    $u_password1=$_POST["pswd"];
 
-    if(!empty($u_username)&&!empty($u_email)&&!empty($u_password))
+    if(!empty($u_username)&&!empty($u_email)&&!empty($u_password1))
     {
-        $hash_password=password_hash($u_password, PASSWORD_DEFAULT);
-       
-        $IDRandom=rand(1,100000);
-        $inserimento="INSERT INTO utente(IdUtente, NickName, email, pass) VALUES ('$IDRandom','$u_username','$u_email','$hash_password')";
-        if ($conn->query($inserimento) === TRUE) {
-            echo "Registrazione completata con successo!";
-            header("Location: ../html/home.html");
-        } else {
-            echo "Errore durante la registrazione: " . $conn->error;
-        }
+            $verifica="SELECT * FROM utente WHERE email='$u_email'";
+            $risultato=$conn->query($verifica);
+            if($risultato->num_rows > 0)
+            {
+                echo "Utente giÃ  registrato, effettua il login" . $conn->error;
+                header("Location: ../html/login.html");
+            }
+            else
+            {
+                $hash_password=md5($u_password1);
+                $IDRandom=rand(1,100000);
+                $_SESSION['userId']=$IDRandom;
+                $inserimento="INSERT INTO utente(IdUtente, NickName, email, pass) VALUES ('$IDRandom','$u_username','$u_email','$hash_password')";
+                if ($conn->query($inserimento) === TRUE) {
+                    echo "Registrazione completata con successo!";
+                    header("Location: ../html/Home.php");
+                } else {
+                    echo "Errore durante la registrazione: " . $conn->error;
+                }   
+            }
+        
     }
     else
     {
